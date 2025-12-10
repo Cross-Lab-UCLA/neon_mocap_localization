add8dec5f527d394b3565f6f04ba84c1d827ab10
Robert Ennis <rob@pupil-labs.com>
2025-12-10 18:57:21 +0100

    add robustness checks to apriltag detection; basic support for cloud data; add a function to get a gaze ray from gaze point

--- apriltags.py
+++ apriltags.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import cv2
 from pupil_apriltags import Detector
 
@@ -19,11 +21,17 @@ class AprilTags:
         self.D = neon.dist_coeffs
         self.tag_size = tag_size
 
+        self.good_detection = True
+        self.reprojection_errors = []
+
+        self.undist_frame = None
         self.tag_poses = []
         self.tag_corners = []
+        self.tag_ids = []
 
         self.detect_tags(img)
-        self.extract_tag_poses()
+        if self.good_detection:
+            self.extract_tag_poses()
 
     def detect_tags(self, image):
         img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -31,7 +39,7 @@ class AprilTags:
         self.new_K, _ = cv2.getOptimalNewCameraMatrix(
             self.K, self.D, img_gray.shape[::-1], 1, img_gray.shape[::-1]
         )
-        undist_frame = cv2.undistort(
+        self.undist_frame = cv2.undistort(
             img_gray, self.K, self.D, None, newCameraMatrix=self.new_K
         )
 
@@ -40,15 +48,49 @@ class AprilTags:
         cx = self.new_K[0, 2]
         cy = self.new_K[1, 2]
 
-        self.at_detection = self.at_detector.detect(
-            undist_frame,
-            estimate_tag_pose=True,
-            tag_size=self.tag_size,
-            camera_params=[fx, fy, cx, cy],
+        try:
+            self.at_detection = self.at_detector.detect(
+                self.undist_frame,
+                estimate_tag_pose=True,
+                tag_size=self.tag_size,
+                camera_params=[fx, fy, cx, cy],
+            )
+        except Exception:
+            self.good_detection = False
+            return
+
+        if len(self.at_detection) < 4:
+            self.good_detection = False
+            return
+
+        tag_points_3d = np.array(
+            [
+                [-self.tag_size / 2, self.tag_size / 2, 0],  # BL
+                [self.tag_size / 2, self.tag_size / 2, 0],  # BR
+                [self.tag_size / 2, -self.tag_size / 2, 0],  # TL
+                [-self.tag_size / 2, -self.tag_size / 2, 0],  # TR
+            ]
         )
 
         for detection in self.at_detection:
+            # SOLVEPNP_IPPE_SQUARE returns 2 solutions for rotation/position/error.
+            # First one always has smallest error
+            ok, (tag_rotation, _), (tag_position, _), (error, _) = cv2.solvePnPGeneric(
+                tag_points_3d,
+                detection.corners,
+                self.K,
+                self.D,
+                flags=cv2.SOLVEPNP_IPPE_SQUARE,
+            )
+
+            if not ok:
+                self.good_detection = False
+                return
+
+            self.reprojection_errors.append(error)
+
             self.tag_corners.append(detection.corners)
+            self.tag_ids.append(detection.tag_id)
 
     def extract_tag_poses(self):
         for detection in self.at_detection:
---
+++ threed_utils.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+
+def unproject_points(points_2d, camera_matrix, distortion_coefs, normalize=False):
+    """
+    Undistorts points according to the camera model.
+    :param pts_2d, shape: Nx2
+    :return: Array of unprojected 3d points, shape: Nx3
+    """
+    # Convert type to numpy arrays (OpenCV requirements)
+    camera_matrix = np.array(camera_matrix)
+    distortion_coefs = np.array(distortion_coefs)
+    points_2d = np.asarray(points_2d, dtype=np.float32)
+
+    # Add third dimension the way cv2 wants it
+    points_2d = points_2d.reshape((-1, 1, 2))
+
+    # Undistort 2d pixel coordinates
+    points_2d_undist = cv2.undistortPoints(points_2d, camera_matrix, distortion_coefs)
+    # Unproject 2d points into 3d directions; all points. have z=1
+    points_3d = cv2.convertPointsToHomogeneous(points_2d_undist)
+    points_3d.shape = -1, 3
+
+    if normalize:
+        # normalize vector length to 1
+        points_3d /= np.linalg.norm(points_3d, axis=1)[:, np.newaxis]
+
+    return points_3d
